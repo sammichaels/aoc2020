65c2e14bf589dcc5ec32bf99d9942ddf3e094b83
sammichaels <sam.michaels@icloud.com>
2020-12-01 23:04:58 -0600

    Added header

--- day1/__init__.py
+++ day1/__init__.py
@@ -1,3 +1,17 @@
+"""This file is part of Advent of Code 2020.
+
+Coded by: Samuel Michaels (samuel.michaels@protonmail.com)
+1 December 2020
+
+NO COPYRIGHT
+This work is dedicated to the public domain.  All rights have been
+waived worldwide under copyright law, including all related and
+neighboring rights, to the extent allowed by law.
+
+You may copy, modify, distribute, and perform the work, even for
+commercial purposes, all without asking permission.  See the
+accompanying COPYRIGHT document."""
+
 import datetime
 import operator
 from functools import reduce
